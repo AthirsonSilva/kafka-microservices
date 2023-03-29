1a5d317117757fb1ce2fb242bfc0d50519c74c9e
azilzor <athirsonarceus@gmail.com>
2023-03-29 19:28:48 -0300

    feat: configuring Order services Kafka topic

---
+++ order-service/src/main/java/microservices/order/config/KafkaTopicConfig.java
@@ -0,0 +1,18 @@
+package microservices.order.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+    @Value("${spring.kafka.topic.name}")
+    private String topicName;
+
+    @Bean
+    public NewTopic orderTopic() {
+        return TopicBuilder.name(topicName).build();
+    }
+}
