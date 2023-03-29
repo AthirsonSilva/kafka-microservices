05e7914e53fbb761ddc997e65e3fde8273e6bef8
azilzor <athirsonarceus@gmail.com>
2023-03-29 20:47:43 -0300

    feat: implementing kafka consumer on Stock service

---
+++ stock-service/src/main/java/microservices/stock/kafka/OrderConsumer.java
@@ -0,0 +1,17 @@
+package microservices.stock.kafka;
+
+import microservices.domains.events.OrderEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderConsumer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderConsumer.class);
+
+    @KafkaListener(topics = "${spring.kafka.topic.name}", groupId = "${spring.kafka.consumer.group-id}")
+    public void consume(OrderEvent order) {
+        LOGGER.info(String.format("Received order event --> %s", order.toString()));
+    }
+}
