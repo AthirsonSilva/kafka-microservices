d274c577e7f299c376640ca435c28b04cb1179ee
azilzor <athirsonarceus@gmail.com>
2023-03-29 19:12:05 -0300

    Initial commit

---
+++ base-domains/src/test/java/microservices/domains/BaseDomainsApplicationTests.java
@@ -0,0 +1,13 @@
+package microservices.domains;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseDomainsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ email-service/src/test/java/microservices/email/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package microservices.email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
